37edebc89a03dc7d9ce5c5e04455d30f15432b31
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2023-08-03 12:24:05 -0500

    added cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,11 +1,15 @@
-import express from 'express'
 import path from 'path'
+import express from 'express'
 import exceptionHandler from 'express-exception-handler'
-exceptionHandler.handle()
-const app = express()
+import cors from 'cors'
 import * as error from '../api/middlewares/error'
 import tokenCheck from '../api/middlewares/tokenCheck'
 import config from './config'
+
+exceptionHandler.handle()
+const app = express()
+
+app.options('*', cors())
 
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))

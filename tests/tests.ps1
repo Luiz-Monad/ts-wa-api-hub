2fca5416c915d0daaff2839908a39d324597f5e4
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2023-08-14 17:33:20 -0500

    node watch

--- tests/tests.ps1
+++ tests/tests.ps1
@@ -1,3 +1,14 @@
 
 Invoke-RestMethod 'http://localhost:3333/instance/init?key=123' -Method 'GET'
-Invoke-RestMethod 'http://localhost:3333/init/text?key=bot' -Method 'POST' -Headers @{"id"="+5518996206006"; "message"="test"} -Body $body
+
+Invoke-RestMethod 'http://localhost:3333/message/text?key=123' -Method 'POST' -Headers  @{"content-type"="application/json"} -body (@{"id"="5518998034361"; "message"="ola"} | ConvertTo-Json)
+
+Invoke-RestMethod 'http://localhost:3333/instance/qr?key=123' -Method 'GET'
+Invoke-RestMethod 'http://localhost:3333/instance/qrbase64?key=123' -Method 'GET'
+Invoke-RestMethod 'http://localhost:3333/instance/qrurl?key=123' -Method 'GET'
+
+(Invoke-RestMethod 'http://localhost:3333/instance/list?key=123' -Method 'GET')
+(Invoke-RestMethod 'http://localhost:3333/instance/list?key=123' -Method 'GET').data
+(Invoke-RestMethod 'http://localhost:3333/instance/qr?key=123' -Method 'GET')
+(Invoke-RestMethod 'http://localhost:3333/instance/info?key=123' -Method 'GET')
+
